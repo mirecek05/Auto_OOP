fb210ea6fbd8d5fec0cb1be80111d64bef31c63a
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-03-01 11:35:36 +0100

    Add project files.

---
+++ Auto_OOP/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auto_OOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Vehicle auto1 = new Vehicle("Škoda");
+            auto1.Type = "Osobní auto";
+
+            Vehicle auto2 = new Vehicle("VW");
+            auto2.Type = "Dodávka";
+        }
+    }
+}
---
+++ Auto_OOP/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_OOP
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string Brand { get; }
+        public string Fuel { get; set; }
+        public double EngineVolume { get; set; }
+        public int FuelLevel { get; set; }
+        public int Distance { get; set; }
+        public int PneuState { get; set; }
+
+        public Vehicle(string brand)
+        {
+            Type = "Unknown";
+            Brand = brand;
+            Fuel = "Gas";
+            EngineVolume = 1000;
+            FuelLevel = 100;
+            Distance = 0;
+            PneuState = 100;
+        }
+    }
+}
